3c5053d942baa41fe9ebf44e7bb2a8f3d7f84553
Giuseppe Ciuni <giuseppe.ciuni@gmail.com>
2025-06-09 17:27:51 +0200

    Bug fixing

--- profiles/models.py
+++ profiles/models.py
@@ -77,7 +77,7 @@ class Project(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.TextField(blank=True)
 	is_active = models.BooleanField(default=True)
-	#metadata = models.JSONField(default=dict, blank=True, null=True)
+	metadata = models.JSONField(default=dict, blank=True, null=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
